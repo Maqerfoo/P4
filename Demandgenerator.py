3bcda31aca2d9bdd445969a4a9efceb5e269fdca
Maqerfoo <63002176+Maqerfoo@users.noreply.github.com>
2020-04-18 09:39:27 +0200

    demand calculations
    Calculated batch quantities of chocolates(batch size of 1875), rounded up to integer. Then I adjusted for batch rollover-effect in the production schedule

--- Demandgenerator.py
+++ Demandgenerator.py
@@ -10,6 +10,9 @@ import pandas as pd
 import datetime
 from scipy.stats import norm
 
+def weeks_in_year_func(year):
+    weeks = datetime.date(year, 12, 28).isocalendar()[1]
+    return weeks
 
 def create_demand_week(avg_demand):
    ''' Create a weekly demand, normally distributed over an average, with sd = 1.10*mean'''
@@ -21,8 +24,8 @@ def create_demand_week(avg_demand):
 def create_demand_year(year, avg_demand_base, mean, sd):
    ''' Create weekly demand for a full year, where the demand increases by  every week, normally distributed with sd=0.0025'''
     #finding weeks in a given year, and creating empty index-array
-   weeks_in_year = datetime.date(year, 12, 28).isocalendar()[1]
-   week_index = ["week0"]
+   weeks_in_year = weeks_in_year_func(2020)
+   week_index = []
    #creating an empty dataframe, for the weekly demands
    demand_year = pd.DataFrame(data = avg_demand_base, index=week_index)
    avg_demand = avg_demand_base.copy()
--- Main.py
+++ Main.py
@@ -8,18 +8,31 @@ Created on Fri Apr 17 10:21:10 2020
 from Demandgenerator import create_demand_year
 import math
 
-
-avg_demand_week0 = {'Lakrids 1' : 4000, 'Lakrids 2' : 4000, 'Lakrids 3' : 4000,
-          'Lakrids 4' : 4000, 'Chocolate A' : 7680, 'Chocolate B' : 7680, 
-          'Chocolate C' : 7680, 'Chocolate D' : 7680, 'Chocolate E' : 7680,
-          'Crispy Caramel' : 6400, 'Blackberry & Dark' : 6400, 
-          'Twisted Banana' : 6400, 'Vanilla Mango' : 6400}   
+avg_demand_week0 = {'Lakrids 1' : 8000, 'Lakrids 2' : 8000, 'Lakrids 3' : 8000,
+          'Lakrids 4' : 8000, 'Chocolate A' : 15360, 'Chocolate B' : 15360, 
+          'Chocolate C' : 15360, 'Chocolate D' : 15360, 'Chocolate E' : 15360,
+          'Crispy Caramel' : 12800, 'Blackberry & Dark' : 12800, 
+          'Twisted Banana' : 12800, 'Vanilla Mango' : 12800}   
 
 
 demand2020 = create_demand_year(2020, avg_demand_week0, mean=0.15, sd=0.025)
 
-week1_lakrids = demand2020.loc[ "week1", demand2020.columns.str.startswith("Lakrids") == True ]
-week1_chocolate = demand2020.loc[ "week1", demand2020.columns.str.startswith("Lakrids") == False ] 
-week1_chocolate_batch = week1_chocolate / 1850
-for i in range(len(week1_chocolate_batch)):
-    week1_chocolate_batch[i] = math.ceil(week1_chocolate_batch[i])
+
+#we can only produce integer number of batches of quantity 1850. So we produce
+#lowest integer, greater than batch quantity. Then we need to adjust for rollover
+#from previous weeks production
+
+lakrids = demand2020.loc[ : , demand2020.columns.str.startswith("Lakrids")]
+chocolate_adjusted_rollover = demand2020.loc[ : , demand2020.columns.str.startswith("Lakrids") == False] 
+chocolate_batch = chocolate_adjusted_rollover / 1875
+chocolate_batch_produced = chocolate_batch.copy()
+for i in range(len(chocolate_batch)):
+    if i == len(chocolate_batch)-1:
+        break
+    for x in range(len(chocolate_batch.columns)):
+        chocolate_batch_produced.iloc[i, x] = math.ceil(chocolate_batch.iloc[i, x])
+        chocolate_adjusted_rollover.iloc[i+1, x] = chocolate_adjusted_rollover.iloc[i+1, x] - (chocolate_batch_produced.iloc[i,x] * 1875 - chocolate_adjusted_rollover.iloc[i,x])
+for i in range(len(chocolate_batch_produced.columns)):
+    chocolate_batch_produced.iloc[-1, i] = math.ceil(chocolate_batch_produced.iloc[-1, i])
+
+
