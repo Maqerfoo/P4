2112c39e6be24c282eea2b4ebb0b223d512f795c
Maqerfoo <63002176+Maqerfoo@users.noreply.github.com>
2020-04-18 12:24:10 +0200

    Bugfixes
    Rollover now works as intended

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Apr 17 10:21:10 2020
-
 @author: jakob
 """
 
@@ -18,21 +17,24 @@ avg_demand_week0 = {'Lakrids 1' : 8000, 'Lakrids 2' : 8000, 'Lakrids 3' : 8000,
 demand2020 = create_demand_year(2020, avg_demand_week0, mean=0.15, sd=0.025)
 
 
-#we can only produce integer number of batches of quantity 1850. So we produce
+#we can only produce integer number of batches of quantity 1875. So we produce
 #lowest integer, greater than batch quantity. Then we need to adjust for rollover
 #from previous weeks production
 
 lakrids = demand2020.loc[ : , demand2020.columns.str.startswith("Lakrids")]
-chocolate_adjusted_rollover = demand2020.loc[ : , demand2020.columns.str.startswith("Lakrids") == False] 
-chocolate_batch = chocolate_adjusted_rollover / 1875
+chocolate = demand2020.loc[ : , demand2020.columns.str.startswith("Lakrids") == False] 
+chocolate_rollover = chocolate.copy()
+for col in chocolate_rollover.columns:
+    chocolate_rollover[col].values[:] = 0
+chocolate_batch = chocolate / 1875
 chocolate_batch_produced = chocolate_batch.copy()
+
 for i in range(len(chocolate_batch)):
-    if i == len(chocolate_batch)-1:
-        break
+    #last week generates no rollover
+    if i == len(chocolate_batch):
+        for i in range(len(chocolate_batch_produced.columns)):
+            chocolate_batch_produced.iloc[-1, i] = math.ceil(chocolate_batch_produced.iloc[-1, i])
+            break
     for x in range(len(chocolate_batch.columns)):
-        chocolate_batch_produced.iloc[i, x] = math.ceil(chocolate_batch.iloc[i, x])
-        chocolate_adjusted_rollover.iloc[i+1, x] = chocolate_adjusted_rollover.iloc[i+1, x] - (chocolate_batch_produced.iloc[i,x] * 1875 - chocolate_adjusted_rollover.iloc[i,x])
-for i in range(len(chocolate_batch_produced.columns)):
-    chocolate_batch_produced.iloc[-1, i] = math.ceil(chocolate_batch_produced.iloc[-1, i])
-
-
+        chocolate_batch_produced.iloc[i, x] = math.ceil(chocolate_batch.iloc[i, x] + (chocolate_rollover.iloc[i, x] / 1875))
+        chocolate_rollover.iloc[i, x] = (chocolate_batch_produced.iloc[i,x] * 1875 - chocolate.iloc[i,x])
